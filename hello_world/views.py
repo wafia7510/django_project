8c9b8efba0ad33b94f6340a94cce6b8fb484d190
Wafia Durrani <wafiadr2@gmail.com>
2025-01-26 01:10:01 +0000

    Add all the file

---
+++ hello_world/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
